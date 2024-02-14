77f965489ece28949953e73730d32a753e17ae79
MohamedTaherBorgi <mohamedtaherborgi@gmail.com>
2024-02-14 18:53:25 +0100

    updates

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegisterController extends AbstractController
+{
+    #[Route('/register', name: 'app_register')]
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+
+            //dd($user); //To analyse what's in $user
+
+            $entityManager->persist($user); //Freeze the data (kinda like -git commit)
+            $entityManager->flush(); //Execute Queries
+        }
+
+        return $this->render('front/register.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
